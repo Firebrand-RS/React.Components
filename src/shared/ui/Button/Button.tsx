713ec8b88c58ed59e426be3145ddfb791f2f3e92
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-10-29 12:27:12 +0300

    feat: add Button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,16 @@
+import React, { ComponentProps } from 'react';
+import classes from './Button.module.scss';
+
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export class Button extends React.Component<ButtonProps> {
+  render() {
+    const { type = 'button', ...props } = this.props;
+
+    return (
+      <button {...props} className={classes.button} type={type}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
