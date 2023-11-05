0dc0ea8e5a6be5aa48224beacaae6a733877359a
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-11-06 00:06:13 +0300

    refactor: convert SubmittableSearch component to FC

--- src/components/SubmittableSearch/SubmittableSearch.tsx
+++ src/components/SubmittableSearch/SubmittableSearch.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from 'react';
+import React, { SyntheticEvent, useState } from 'react';
 import classes from './SubmittableSearch.module.scss';
 import { Button } from '../Button/Button';
 
@@ -7,53 +7,35 @@ interface SubmittableSearchProps {
   value: string;
   onSearch: (value: string) => void;
 }
-interface SubmittableSearchState {
-  value: string;
-}
 
-export class SubmittableSearch extends React.Component<
-  SubmittableSearchProps,
-  SubmittableSearchState
-> {
-  constructor(props: SubmittableSearchProps) {
-    super(props);
-    this.state = {
-      value: props.value,
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+export function SubmittableSearch({
+  value: valueProp,
+  onSearch,
+  placeholder,
+}: SubmittableSearchProps) {
+  const [value, setValue] = useState(valueProp);
 
-  handleChange(evt: React.ChangeEvent<HTMLInputElement>): void {
+  function handleChange(evt: React.ChangeEvent<HTMLInputElement>): void {
     const { target } = evt;
-    this.setState({ value: target.value });
+    setValue(target.value);
   }
 
-  handleSubmit(evt: SyntheticEvent) {
+  function handleSubmit(evt: SyntheticEvent): void {
     evt.preventDefault();
-    this.props.onSearch(this.state.value);
+    onSearch(value);
   }
 
-  setValue(value: string) {
-    this.setState({ value });
-    this.props.onSearch(value);
-  }
-
-  render() {
-    const { placeholder } = this.props;
-
-    return (
-      <form className={classes.search} onSubmit={this.handleSubmit}>
-        <input
-          className={classes.input}
-          type="search"
-          name="search-field"
-          placeholder={placeholder}
-          onChange={this.handleChange}
-          value={this.state.value}
-        />
-        <Button type="submit">Search</Button>
-      </form>
-    );
-  }
+  return (
+    <form className={classes.search} onSubmit={handleSubmit}>
+      <input
+        className={classes.input}
+        type="search"
+        name="search-field"
+        placeholder={placeholder}
+        onChange={handleChange}
+        value={value}
+      />
+      <Button type="submit">Search</Button>
+    </form>
+  );
 }
