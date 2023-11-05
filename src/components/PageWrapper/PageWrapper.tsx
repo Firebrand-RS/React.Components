25d16a1054999581e978df4179ffd6082bfee261
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-11-06 01:23:13 +0300

    refactor: convert PageWrapper class component to FC

--- src/components/PageWrapper/PageWrapper.tsx
+++ src/components/PageWrapper/PageWrapper.tsx
@@ -3,8 +3,6 @@ import classes from './PageWrapper.module.scss';
 
 interface PageWrapperProps extends ComponentProps<'div'> {}
 
-export class PageWrapper extends React.Component<PageWrapperProps> {
-  render() {
-    return <div {...this.props} className={classes.wrapper}></div>;
-  }
+export function PageWrapper(props: PageWrapperProps) {
+  return <div {...props} className={classes.wrapper}></div>;
 }
