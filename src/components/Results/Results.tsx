0fc2a9e8e7ce2fa0ced8bc522620c34e2eb12756
Dima Grigorev <firebrand-rsschool@rambler.ru>
2023-11-06 16:57:31 +0300

    feat: add no results message

--- src/components/Results/Results.tsx
+++ src/components/Results/Results.tsx
@@ -1,10 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, { Children, ReactNode } from 'react';
 import classes from './Results.module.scss';
 import { Button } from '../Button/Button';
 import { Pagination } from '../Pagination/Pagination';
 
 const PAGINATION_BUTTON_PAIR_COUNT = 2;
 const PAGINATION_BUTTON_GAP = 8;
+const NO_RESULTS_MESSAGE = `Sorry, we couldn't find any results`;
 
 interface ResultsProps {
   page: number;
@@ -14,9 +15,15 @@ interface ResultsProps {
 }
 
 function Results({ page, totalPages, onNavigate, children }: ResultsProps) {
+  const hasResults = Children.count(children) > 0;
+
   return (
     <>
-      <div className={classes.content}>{children}</div>
+      {hasResults ? (
+        <div className={classes.content}>{children}</div>
+      ) : (
+        <NoResults />
+      )}
       <Pagination
         currentPage={page}
         pageCount={totalPages}
@@ -27,6 +34,10 @@ function Results({ page, totalPages, onNavigate, children }: ResultsProps) {
       />
     </>
   );
+}
+
+function NoResults() {
+  return <p className={classes.noresults}>{NO_RESULTS_MESSAGE}</p>;
 }
 
 export default Results;
